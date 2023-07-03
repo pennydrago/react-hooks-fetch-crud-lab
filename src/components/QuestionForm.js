aa5907720105823037841413ba37ddda338cd888
Penelope Drago <prd@drago.com.au>
2023-07-03 11:45:02 +1000

    add CRUD features

--- src/components/QuestionForm.js
+++ src/components/QuestionForm.js
@@ -1,14 +1,17 @@
 import React, { useState } from "react";
 
 function QuestionForm(props) {
-  const [formData, setFormData] = useState({
+  
+  const initialFormData = {
     prompt: "",
     answer1: "",
     answer2: "",
     answer3: "",
     answer4: "",
     correctIndex: 0,
-  });
+  }
+  
+  const [formData, setFormData] = useState(initialFormData);
 
   function handleChange(event) {
     setFormData({
@@ -19,7 +22,21 @@ function QuestionForm(props) {
 
   function handleSubmit(event) {
     event.preventDefault();
-    console.log(formData);
+    const itemData = {
+      "prompt": formData.prompt,
+      "answers": [formData.answer1, formData.answer2, formData.answer3, formData.answer4],
+      "correctIndex": formData.correctIndex
+    }
+    fetch("http://localhost:4000/questions", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(itemData)
+    })
+      .then((response) => response.json())
+      .then((newItem) => console.log(newItem));
+    setFormData(initialFormData);
   }
 
   return (
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,38 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import QuestionItem from "./QuestionItem";
 
 function QuestionList() {
+  const [questionItems, setQuestionItems] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+      .then((response) => response.json())
+      .then((questionItems) => setQuestionItems(questionItems));
+  }, []);
+
+  function handleUpdateAnswer(updatedQuestionItem) {
+    const updatedQuestionItems = questionItems.map((questionItem) => {
+      if (questionItem.id === updatedQuestionItem.id) {
+        return updatedQuestionItem;
+      } else {
+        return questionItem;
+      }
+    });
+    setQuestionItems(updatedQuestionItems);
+  }
+
+  function handleDeleteQuestion(deletedQuestion) {
+    const updatedQuestionItems = questionItems.filter((questionItem) => questionItem.id !== deletedQuestion.id);
+    setQuestionItems(updatedQuestionItems);
+  }
+  
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{/* display QuestionItem components here after fetching */}
+      {questionItems.map((questionItem) => {
+      return <QuestionItem key={questionItem.id} question={questionItem} onUpdateAnswer={handleUpdateAnswer} onDeleteQuestion={handleDeleteQuestion} />})}
+      </ul>
     </section>
   );
 }
